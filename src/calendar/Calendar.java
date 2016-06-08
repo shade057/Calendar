10743e5b53f8ab804eda205b607feb6cca14a8f8
fgala <fgala@DESKTOP-EQLDCVJ>
2016-06-08 13:30:24 -0700

    Hello World Calendar Version

---
+++ src/calendar/Calendar.java
@@ -0,0 +1,11 @@
+
+package calendar;
+
+/* Simple Hello World program used to test git functionality*/
+
+public class Calendar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
